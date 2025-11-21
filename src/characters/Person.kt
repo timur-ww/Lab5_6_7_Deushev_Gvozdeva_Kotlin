6a8282edc057a29689a2519e99f0bec51dccee45
timurqqe <timurkkq@gmail.com>
2025-11-21 18:14:38 +0300

    Update Person.kt

--- src/characters/Person.kt
+++ src/characters/Person.kt
@@ -1,5 +1,6 @@
 package characters
-
+import world.Quest
+import world.QuestType
 import kotlin.math.E
 
 fun main() {
@@ -12,7 +13,7 @@ fun main() {
     println(dragon.dropLoot())
     dragon.takeDamage(150)*/
 
-    val enemy: GameCharacter = Enemy("Джин")
+    /*val enemy: GameCharacter = Enemy("Джин")
     val hero: GameCharacter = Hero("Наруто")
     val npc: GameCharacter = NPC("Торговец")
 
@@ -20,5 +21,23 @@ fun main() {
 
     (enemy as Enemy).takeDamage(20)
     enemy.takeDamage(20)
-    println(enemy.name)
+    println(enemy.name)*/
+
+    /*val trader = Trader("Ральф")
+
+    trader.addQuest(Quest("Собрать травы", 2, 50,"Легкий", QuestType.DELIVERY))
+    trader.addQuest(Quest("Убить волков", 3, 100, "Средний", QuestType.ELIMINATION))
+
+    trader.showAvailableQuests()
+
+    val selectedQuest = trader.giveQuest(1)
+    selectedQuest.describe()*/
+
+    // Самостоятельные задания lab-8
+
+    // Задание 2: Работа с системой ввода-вывода
+
+   val trader = Trader()
+   trader.start()
 }
+
