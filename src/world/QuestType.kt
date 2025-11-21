9ee11702aedd683b7b845ed3d042441dfcf13f06
timurqqe <timurkkq@gmail.com>
2025-11-21 18:12:05 +0300

    Delete src/world/QuestType.kt

--- src/world/QuestType.kt
+++
@@ -1,9 +0,0 @@
-package world
-
-enum class QuestType(val description: String) {
-    DELIVERY("Доставка предмета"),
-    ELIMINATION("Устранение противника"),
-    ESCORT("Сопровождение персонажа"),
-    EXPLORE("Исследование новой территории"),
-    BOSSFIGHT("Битва с боссом")
-}
